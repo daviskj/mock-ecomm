deaa5e50b665ebdeefd71372a3442136ffbed7d2
Kyle Davis <kdavisj@outlook.com>
2023-03-14 02:50:30 -0400

    product page

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assets/1.png'
+import product2 from './assets/2.png'
+import product3 from './assets/3.png'
+import product4 from './assets/4.png'
+import product5 from './assets/5.png'
+import product6 from './assets/6.webp'
+import product7 from './assets/7.webp'
+import product8 from './assets/8.webp'
+
+
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "Sword",
+      price: 200.0,
+      productImage: product1,
+    },
+    {
+      id: 2,
+      productName: "NVIDIA GeForce RTX 3090",
+      price: 1500.0,
+      productImage: product2,
+    },
+    {
+      id: 3,
+      productName: "Cannon M50 Camera",
+      price: 699.0,
+      productImage: product3,
+    },
+    {
+      id: 4,
+      productName: "Nike Jacket",
+      price: 228.0,
+      productImage: product4,
+    },
+    {
+      id: 5,
+      productName: "LED Light Strips",
+      price: 19.99,
+      productImage: product5,
+    },
+    {
+      id: 6,
+      productName: "Surface Pro 8",
+      price: 1019.0,
+      productImage: product6,
+    },
+    {
+      id: 7,
+      productName: "Wilson Dynapower Hybrid Golfclub",
+      price: 340.0,
+      productImage: product7,
+    },
+    {
+      id: 8,
+      productName: "Adidas Hat",
+      price: 24.99,
+      productImage: product8,
+    },
+  ];
